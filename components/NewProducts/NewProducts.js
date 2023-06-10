dd9ce595ee2cb7342697849b53e101a2ce69d19a
AntonioJesusRG <antoniogar129@gmail.com>
2023-06-10 19:20:03 +0200

    Add ProductBox component

--- components/NewProducts/NewProducts.js
+++ components/NewProducts/NewProducts.js
@@ -1,14 +1,17 @@
+import ProductBox from '../ProductBox/ProductBox'
 import style from './NewProducts.module.scss'
 
 export default function NewProducts ({newProducts}){
-  return(
-    <section className={style.section}>
-      <div className={style.sectionGrid}>
-        {newProducts?.length > 0 ? 
-        newProducts.map(newProduct => (
-        <div key={newProduct.id}>{newProduct.title}</div>)) 
-        : null}
-    </div>
-    </section>
-  )
+  return (
+		<section className={style.section}>
+			<h1 className={style.sectionTitle}>New products</h1>
+			<div className={style.sectionGrid}>
+				{newProducts?.length > 0
+					? newProducts.map((newProduct) => (
+							<ProductBox key={newProduct.id} {...newProduct}></ProductBox>
+					  ))
+					: null}
+			</div>
+		</section>
+	);
 }
