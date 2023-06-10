ac3cc04076f4ad9986d177f0f2953222b7ec81fe
AntonioJesusRG <antoniogar129@gmail.com>
2023-06-10 19:21:19 +0200

    Improve Button component

--- components/PrimaryBtn/PrimaryBtn.js
+++ components/PrimaryBtn/PrimaryBtn.js
@@ -1,6 +1,14 @@
+import Link from "next/link";
 import style from "./PrimaryBtn.module.scss"
 
-export default function PrimaryBtn({ type, children, href}) {
-	const buttonClasses = `${style.button} ${style[type]}`;
-	return <button href={href} className={buttonClasses}>{children}</button>;
+export default function PrimaryBtn({ type, children, href, size}) {
+	const buttonClasses = `${style.button} ${style[type]} ${style[size]}`;
+
+	return href !== undefined ? (
+		<Link href={href} className={buttonClasses}>
+			{children}
+		</Link>
+	) : (
+		<button className={buttonClasses}>{children}</button>
+	);
 }
