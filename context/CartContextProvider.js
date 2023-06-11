1ed1b95845782dabf99fc9cd3b132346ea9654f7
AntonioJesusRG <antoniogar129@gmail.com>
2023-06-11 02:04:39 +0200

    Cart data persistence

--- context/CartContextProvider.js
+++ context/CartContextProvider.js
@@ -1,11 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CartContext } from "./CartContext";
 
 export function CartContextProvider({ children }) {
 	const [cartProducts, setCartProducts] = useState([]);
 	const addProduct = (productId) =>{
-		setCartProducts(prev => [...prev, productId])
+		setCartProducts(prev => [...prev, productId]);
 	}
+
+	useEffect(()=>{
+		setCartProducts(JSON.parse(localStorage.getItem('cart')) || []);
+	},[])
+
+	useEffect(()=>{
+		if(cartProducts.length>0){
+			localStorage.setItem('cart', JSON.stringify(cartProducts));
+		}
+	},[cartProducts])
+
 	return (
 		<CartContext.Provider value={{ cartProducts, addProduct }}>
 			{children}
