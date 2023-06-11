d462ce8e99e93dcec4d11949fb6488f91fa39f19
AntonioJesusRG <antoniogar129@gmail.com>
2023-06-11 02:20:33 +0200

    Get info of cart products from DB

--- pages/cart.js
+++ pages/cart.js
@@ -3,27 +3,33 @@ import Header from "@/components/Header/Header";
 import style from '../styles/pagesStyle/cart.module.scss'
 import { useContext, useEffect, useState } from "react";
 import { CartContext } from "@/context/CartContext";
+import axios from "axios";
 
 export default function CartPage () {
   const { cartProducts } = useContext(CartContext);
   const [products, setProducts] = useState([])
 
   useEffect(() => {
-    
+    axios.post('/api/cart', {ids: cartProducts}).then(response =>{
+      setProducts(response.data)
+    })
   }, [cartProducts]);
 
   return (
 		<>
 			<Header></Header>
 			<article className={style.cartContainer}>
-				<section className={style.cartList + ' ' + style.cartBox}>
+				<section className={style.cartBox}>
 					<h2>Cart</h2>
-					{cartProducts.length > 0 ? 
-            cartProducts.map(cartProduct=> (
-            <div key={cartProduct}>{cartProduct}</div>))
-            : 
-            <div>Your cart is empty.</div>
-          }
+					<div className={style.cartList}>
+						{products.length > 0 ? (
+							products.map((product) => (
+								<div key={product.id}>{product.title}</div>
+							))
+						) : (
+							<div>Your cart is empty.</div>
+						)}
+					</div>
 				</section>
 				<section className={style.orderInfo + ' ' + style.cartBox}>
 					<h2>Order Information</h2>
