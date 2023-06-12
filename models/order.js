22312c25bb82bc7f83736e1bf05760ff0d0f1b1f
AntonioJesusRG <antoniogar129@gmail.com>
2023-06-12 18:19:42 +0200

    Fix model name

--- models/order.js
+++ models/order.js
@@ -1,18 +1,20 @@
-import { Schema, model } from "mongoose";
+import { Schema, model, models } from "mongoose";
 
-const orderSchema = new Schema({
-  lineItems: Object,
-  name: String,
-  email: String,
-  city: String,
-  postalCode: String,
-  street: String,
-  city: String,
-  country: String,
-  paid: Boolean,
+const orderSchema = new Schema(
+	{
+		lineItems: Object,
+		name: String,
+		email: String,
+		city: String,
+		postalCode: String,
+		street: String,
+		city: String,
+		country: String,
+		paid: Boolean,
+	},
+	{
+		timestamps: true,
+	}
+);
 
-},{
-  timestamps: true,
-})
-
-export const ProductOrder = models?.ProductOrder || model('Order', orderSchema)
+export const ProductOrder = models?.ProductOrder || model('ProductOrder', orderSchema);
