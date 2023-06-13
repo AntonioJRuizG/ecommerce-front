af215904b79832efccbfba2ec6d557bec3b3e340
AntonioJesusRG <antoniogar129@gmail.com>
2023-06-13 15:43:12 +0200

    Change and improve general style

--- components/Featured/Featured.js
+++ components/Featured/Featured.js
@@ -32,6 +32,8 @@ export default function Featured ({product}) {
 					className={style.sectionImg}
 					src='https://firebasestorage.googleapis.com/v0/b/next-ecommerce-fdf78.appspot.com/o/products%2FIphone%2014-3306?alt=media&token=c5bb9734-d6c5-40b3-a5ad-715693cae0e1'
 					alt='Featured product'
+					width={300}
+					height={300}
 				></img>
 			</div>
 		</section>
