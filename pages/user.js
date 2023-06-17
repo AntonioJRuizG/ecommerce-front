bebdfa10047cf072f571a513e1ee321688db7246
AntonioJesusRG <antoniogar129@gmail.com>
2023-06-17 19:15:09 +0200

    Add user page

---
+++ pages/user.js
@@ -0,0 +1,45 @@
+import Header from '@/components/Header/Header';
+
+import style from '../styles/pagesStyle/user.module.scss';
+import { useContext } from 'react';
+import { UserContext } from '@/context/UserContext';
+import { useRouter } from 'next/router';
+import PrimaryBtn from '@/components/PrimaryBtn/PrimaryBtn';
+
+export default function UserPage() {
+	const router = useRouter();
+	const { currentUser, removeUser } = useContext(UserContext);
+
+  const handleLogoutClick = () => {
+    removeUser();
+		router.push('/login');
+	};
+
+  const handleWishlistClick = () => {
+    router.push('/wishlist');
+  };
+
+  const handleCartClick = () => {
+    router.push('/cart');
+  };
+
+	return (
+		<>
+			<Header></Header>
+			<section className={style.section}>
+				<div className={style.sectionUserInfo}>
+					<h2 className={style.sectionTitle}>Hello, {currentUser.name}</h2>
+					<PrimaryBtn onClick={handleCartClick} btn='secondaryBtn'>
+						Your cart
+					</PrimaryBtn>
+					<PrimaryBtn onClick={handleWishlistClick} btn='secondaryBtn'>
+						Your wishlist
+					</PrimaryBtn>
+					<PrimaryBtn onClick={handleLogoutClick} btn='tertiaryBtn'>
+						Log out
+					</PrimaryBtn>
+				</div>
+			</section>
+		</>
+	);
+}
